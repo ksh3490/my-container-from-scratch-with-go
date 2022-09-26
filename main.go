1fdb9bc95a2bb9dc79c0e3ba72a15ba32c36d602
ksh3490 <ksh349021@gmail.com>
2022-09-26 13:15:23 +0900

    Add child function and Need to fix 'operation not permitted' error

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 // docker 				run <image> <cmd> <params>
@@ -13,6 +14,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("Help!")
 	}
@@ -21,12 +24,26 @@ func main() {
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		// Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+	syscall.Sethostname([]byte("my-container"))
+
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	must(cmd.Run())
 
+	must(cmd.Run())
 }
 
 func must(err error) {
