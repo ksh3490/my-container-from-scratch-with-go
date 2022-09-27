de7424257cf6a021d0c684d159fa57d2ab4f012d
ksh3490 <ksh349021@gmail.com>
2022-09-27 20:12:43 +0900

    Add clone flag syscall.CLONE_NEWUTS and syscall.CLONE_NEWPID

--- main.go
+++ main.go
@@ -22,20 +22,21 @@ func main() {
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 	must(cmd.Run())
 }
 
 func child() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+
 	syscall.Sethostname([]byte("my-container"))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
