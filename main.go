e25b62f6d7e31b39aee9b7c7f1cdf33fd1bfb2bf
ksh3490 <ksh349021@gmail.com>
2022-09-28 15:57:02 +0900

    Add syscall.Chdir to change its current dir when a container starts | Add syscall.Mount to isolate a process of a container with a host

--- main.go
+++ main.go
@@ -29,7 +29,8 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
 	must(cmd.Run())
 }
@@ -38,6 +39,8 @@ func child() {
 	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
 
 	syscall.Sethostname([]byte("my-container"))
+	must(syscall.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
@@ -45,6 +48,7 @@ func child() {
 	cmd.Stderr = os.Stderr
 
 	must(cmd.Run())
+	syscall.Unmount("proc", 0)
 }
 
 func must(err error) {
